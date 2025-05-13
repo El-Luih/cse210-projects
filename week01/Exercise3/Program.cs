3b136aa2fa88bd9f3eeb99658af5d71eb7077270
El-Luih <luisalejandro632@gmail.com>
2025-05-13 06:00:14 -0600

    C# programming exercises.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random magicGenerator = new Random();
+        int magicNumber = magicGenerator.Next(1, 101);
+
+        Console.WriteLine("Guess the magic number between 1 and 100!");
+
+        bool isGuessed = false;
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("That's the magic number!");
+                isGuessed = true;
+            }
+
+        } while (!isGuessed);
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<float> numbersList = new List<float>();
+
+        float newNumber;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            newNumber = float.Parse(Console.ReadLine());
+
+            if (newNumber != 0)
+            {
+                numbersList.Add(newNumber);
+            }
+
+        } while (newNumber != 0);
+
+        float totalSum = 0;
+        float largestNumber = 0;
+
+        foreach (float number in numbersList)
+        {
+            totalSum += number;
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+        }
+
+        float average = totalSum / numbersList.Count;
+
+        Console.WriteLine($"The sum of the numbers is: {totalSum}");
+        Console.WriteLine($"The average of the numbers is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
