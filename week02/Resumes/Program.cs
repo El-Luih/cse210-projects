a8458599a3bba13325dfa5efcf5c37633de9f542
El-Luih <luisalejandro632@gmail.com>
2025-05-19 18:04:58 -0600

    Abstraction assignment.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Sula";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Leyde";
+        job2._jobTitle = "System Administrator";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Luis Maradiaga";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
