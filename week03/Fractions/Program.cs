98f2e89f6c95a454fdcf0a9a1ad85492f756c04d
El-Luih <luisalejandro632@gmail.com>
2025-05-26 05:49:05 -0600

    Encapsulation exercise.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac1 = new Fraction();
+        frac1.SetTop(1);
+        frac1.SetBottom(2);
+        Console.WriteLine(frac1.GetTop());
+        Console.WriteLine(frac1.GetBottom());
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        Fraction frac2 = new Fraction(8);
+        Console.WriteLine(frac2.GetTop());
+        Console.WriteLine(frac2.GetBottom());
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(8, 98);
+        Console.WriteLine(frac3.GetTop());
+        Console.WriteLine(frac3.GetBottom());
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
