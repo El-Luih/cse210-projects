ec2ebf2498b4da594a873c63187fd4c558543aee
El-Luih <luisalejandro632@gmail.com>
2025-06-06 02:35:44 -0600

    Inheritance Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment newAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine($"{newAssignment.GetSummary()}\n");
+
+        MathAssignment newMath = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(newMath.GetSummary());
+        Console.WriteLine($"{newMath.GetHomerworkList()}\n");
+
+        WritingAssignment newWriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(newWriting.GetSummary());
+        Console.WriteLine(newWriting.GetWritingInformation());
     }
 }
